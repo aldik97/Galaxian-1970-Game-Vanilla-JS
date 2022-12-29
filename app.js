7e7c035081840d364cfec1ef67b11d93ef9b2b33
aldik7 <aldikh.ak@gmail.com>
2022-12-29 07:01:15 +0700

    commit 1

---
+++ app.js
@@ -0,0 +1,86 @@
+// Set up canvas
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+// Set up game variables
+let playerX = 50;
+let playerY = canvas.height - 50;
+let playerSpeed = 5;
+
+let alienSpeed = 2;
+let alienDirection = 1;
+
+let aliens = [];
+for (let i = 0; i < 10; i++) {
+    aliens.push({
+        x: i * 50,
+        y: 50,
+    });
+}
+
+// Set up game loop
+function gameLoop() {
+    // Clear canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw player
+    ctx.fillStyle = 'white';
+    ctx.fillRect(playerX, playerY, 30, 30);
+
+    // Draw aliens
+    ctx.fillStyle = 'red';
+    aliens.forEach(alien => {
+        ctx.fillRect(alien.x, alien.y, 30, 30);
+    });
+
+    // Move player
+    if (input.left) {
+        playerX -= playerSpeed;
+    }
+    if (input.right) {
+        playerX += playerSpeed;
+    }
+
+    // Move aliens
+    aliens.forEach(alien => {
+        alien.x += alienSpeed * alienDirection;
+    });
+
+// Check if aliens have reached the edge of the screen
+    const rightmostAlien = aliens[aliens.length - 1];
+    if (rightmostAlien.x > canvas.width - 30 || aliens[0].x < 0) {
+        alienDirection *= -1;
+        aliens.forEach(alien => {
+            alien.y += 30;
+        });
+    }
+    // Request another frame
+    requestAnimationFrame(gameLoop);
+}
+
+// Set up input tracking
+const input = {
+    left: false,
+    right: false,
+};
+
+document.addEventListener('keydown', event => {
+    if (event.code === 'ArrowLeft') {
+        input.left = true;
+    }
+    if (event.code === 'ArrowRight') {
+        input.right = true;
+    }
+});
+document.addEventListener('keyup', event => {
+    if (event.code === 'ArrowLeft') {
+        input.left = false;
+    }
+    if (event.code === 'ArrowRight') {
+        input.right = false;
+    }
+});
+
+// Start game loop
+requestAnimationFrame(gameLoop);
+
